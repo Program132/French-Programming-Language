071734440a7a7cb7e309cbee23086f4141697680
Program <67320703+Program132@users.noreply.github.com>
2022-04-14 19:28:50 +0200

    Ajout d'une nouvelle fonctionnalité: /n

--- headers/Management_Envoyer.h
+++ headers/Management_Envoyer.h
@@ -13,8 +13,20 @@ void FPL_Print_Init(std::vector<std::string>& content_line_slitted, std::string&
 	std::string whatReplace = "envoyer ";
 	std::string print_content = replaceFPL(line, whatReplace);
 
-
 	// Envoyer 
+    std::vector<std::string> vec = split(print_content);
 
-	std::cout << print_content << std::endl;
+    int i = 0;
+    for (auto const e : vec) {
+        if (e == "/n") {
+            vec[i] = "";
+            std::cout << "\n";
+            continue;
+        }
+        std::cout << e << " ";
+        i++;
+    }
+    std::cout << "\n";
+
+	//std::cout << print_content << std::endl;
 }
