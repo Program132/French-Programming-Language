95a1fe8dda38f38614efbccb9ef62af25203ac40
Program <67320703+Program132@users.noreply.github.com>
2022-07-18 11:45:03 +0200

    Fixs

--- main.cpp
+++ main.cpp
@@ -1,29 +1,38 @@
 #include "src/TokenBuilding.h"
 #include "src/Parser.h"
 #include <iostream>
+#include <string>
 #include <fstream>
 
 int main() {
     //std::cout << "F.P.L. : Chargement\n\n" << std::endl;
 
-    std::ifstream file {"../main.fpl"};
-    if (!file) { std::cerr << "Fichier introuvable." << std::endl; return 1; }
+    std::string fileName;
+    std::cout << "Fichier : ";
+    std::cin >> fileName;
+    std::cout << std::endl;
 
-    std::string f_content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+    std::ifstream file { fileName};
+    std::ifstream file2 {"../" + fileName};
+    if (!file && !file2) { std::cerr << "Fichier introuvable." << std::endl; return 1; }
 
-    //std::cout << f_content << std::endl;
-
-    FPL::TokenBuilding t;
-    std::vector<FPL::Token> tokens = t.parseToken(f_content);
-
-    for (auto &token : tokens) {
-        //token.DebugPrint();
+    if (!file2) {
+        std::string f_content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+        FPL::TokenBuilding t;
+        std::vector<FPL::Token> tokens = t.parseToken(f_content);
+        FPL::Parser parser;
+        parser.parse(tokens);
+        parser.DebugPrint();
+    } else if (!file) {
+        std::string f_content((std::istreambuf_iterator<char>(file2)), (std::istreambuf_iterator<char>()));
+        FPL::TokenBuilding t;
+        std::vector<FPL::Token> tokens = t.parseToken(f_content);
+        FPL::Parser parser;
+        parser.parse(tokens);
+        parser.DebugPrint();
     }
 
-    FPL::Parser parser;
-    parser.parse(tokens);
-
-    parser.DebugPrint();
-
+    char c;
+    std::cin >> c;
     return 0;
 }
